1b26c58df56eb0c0d5f5c956c7622690d6c92258
s12340146-droid <s12340146@stu.najah.edu>
2026-08-02 10:35:08 +0100

    test8

--- src/test/java/com/vrms/domain/VehicleTypeRulesTest.java
+++ src/test/java/com/vrms/domain/VehicleTypeRulesTest.java
@@ -10,21 +10,7 @@ import org.junit.jupiter.api.Test;
 
 class VehicleTypeRulesTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
 
 	@Test
     public void car_shouldReturnCarType() {
